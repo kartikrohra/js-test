3dc96cb3cccfece4c7cd70339aadc352dab4358f
kartikrohra <kartikrohra2006@gmail.com>
2026-07-04 08:35:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const AccountId = 14335
+let AccountEmail = "kartik@gmail.com"
+var password = "1234"
+password = "5678"
+let Accountstate;
+console.log(password);
+AccountEmail = "kartik2310@gmail.com"
+console.log(AccountEmail)
+ console.log(AccountId);
+ console.table([AccountId,AccountEmail,password,Accountstate])
+  
