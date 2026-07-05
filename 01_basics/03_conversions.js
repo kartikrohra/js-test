ffc42373371c8987c316e66de580436d3882bfc3
kartikrohra <kartikrohra2006@gmail.com>
2026-07-05 13:43:39 +0000

    conversion

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,8 @@
+let score = '33aa'
+console.log(typeof score)
+let value = Number(score)
+console.log(typeof value)
+//when u save something like this 33aa and perform conversion , 
+// it will show type as number but inside value is NaN ie. not a number.
+console.log( value)
+//same goes for all 
