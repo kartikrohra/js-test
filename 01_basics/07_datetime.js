347be16f1691f0b720efd57cae14de7a645d80b1
kartikrohra <kartikrohra2006@gmail.com>
2026-07-17 15:30:47 +0000

    nums and datetime

---
+++ 01_basics/07_datetime.js
@@ -0,0 +1,15 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString()); 
+let Createdata = new Date(2023,2,23)
+console.log(Createdata.toDateString());
+
+
+
+
+
+
+
+ 
